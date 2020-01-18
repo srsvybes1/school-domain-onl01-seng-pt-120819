ea66cd605d2bf2e018d4eb0f21b2955fa027868e
Cody Toney-Griffith <cody.toney@gmail.com>
2020-01-18 03:15:49 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -4,9 +4,10 @@ class School
   ROSTER = []
   def initialize(roster)
     @roster= ROSTER
-    ROSTER << add_student
+    
     end
 def add_student
   @add_student = add_student
+  ROSTER << add_student
   end
   end
