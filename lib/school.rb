3be29182e59e68b7e0d12b1c95ecf295a10105cd
Cody Toney-Griffith <cody.toney@gmail.com>
2020-01-18 03:19:40 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -2,8 +2,10 @@ class School
   attr_accessor :school, :add_student
   attr_reader :roster, :school, :add_student
   ROSTER = []
-  def initialize(roster)
+  def initialize(roster, add_student)
     @roster= ROSTER
+    @add_student = add_student
+    
     end
 def add_student=(add_student)
   @add_student = add_student
