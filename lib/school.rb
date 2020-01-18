2d1c589777f114697c9cf2d0b34d5a4f809ec866
Cody Toney-Griffith <cody.toney@gmail.com>
2020-01-18 03:39:07 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,11 @@
-# code here!
+class School
+  attr_accessor :add_student, :roster
+  attr_reader :roster 
+  ROSTER = []
+  def initialize(roster)
+    @roster= ROSTER
+    end
+def add_student (add_student, grade)
+  @add_student = add_student
+  @grade = grade
+  end
